44dadabb759cc5c91cebab935f0cf387e4e1bd57
Ramiro Cabrera <55851332+RGustavoCM@users.noreply.github.com>
2022-09-28 13:49:42 -0500

    Update Ejercicio#1.js

--- Ejercicio#1.js
+++ Ejercicio#1.js
@@ -6,13 +6,16 @@ var pt1 = (date_str.match(/(....)(..)(..)(..)/).slice(1,4).join('/'));
 var pt2 = (date_str.match(/(....)(..)(..)(..)(..)(..)(..)/).slice(4,7).join(':'));
 
 var final = pt1 + " " +pt2;
+
 const format1 = "YYYY-MM-DD HH:mm:ss"
 const format2 = "YYYY-MM-DD"
+
 var date1 = new Date(final);
 var date2 = new Date(pt1);
 
 const dateTime1 = moment(date1).format(format1);
-const dateTime2 = moment(date2).format(format2);
 
-console.log(dateTime1);
-console.log(dateTime2);
+console.log("********************************");
+console.log("**          Fecha            **");
+console.log("**  ",dateTime1, "  ** ");
+console.log("********************************");
