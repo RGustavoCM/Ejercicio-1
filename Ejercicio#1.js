1d997c565796a1cc67b46d3ca9f807ca2f9b2716
Ramiro Cabrera <55851332+RGustavoCM@users.noreply.github.com>
2022-09-28 19:32:56 -0500

    Update Ejercicio#1.js

--- Ejercicio#1.js
+++ Ejercicio#1.js
@@ -1,21 +1,17 @@
-const moment = require('moment');
+const date_str = '20210620T114039937Z';
+//                2021-06-20T11:40:39.937Z
 
-const date_str = '202209082014525070000';
-
-var pt1 = (date_str.match(/(....)(..)(..)(..)/).slice(1,4).join('/'));
-var pt2 = (date_str.match(/(....)(..)(..)(..)(..)(..)(..)/).slice(4,7).join(':'));
-
-var final = pt1 + " " +pt2;
-
-const format1 = "YYYY-MM-DD HH:mm:ss"
-const format2 = "YYYY-MM-DD"
+var pt1 = (date_str.match(/(....)(..)(.....)(..)(..)(....)/).slice(1,4).join('-'));
+var pt2 = (date_str.match(/(....)(..)(.....)(..)(..)(....)/).slice(4,6).join(':'));
+var pt3 = (date_str.match(/(....)(..)(.....)(..)(..)(....)/).slice(6,8).join(':'));
+var final = pt1 + ":" + pt2 + "." + pt3;
 
 var date1 = new Date(final);
-var date2 = new Date(pt1);
+var date2 = new Date(final).toUTCString();
+var date3 = new Date(final).toISOString();
 
-const dateTime1 = moment(date1).format(format1);
+console.log(" Local Time :  " + date1);
+console.log(" UTC Time :  " + date2);
+console.log(" ISO-8601 format :  " + date3);
 
-console.log("********************************");
-console.log("**          Fecha            **");
-console.log("**  ",dateTime1, "  ** ");
-console.log("********************************");
+
